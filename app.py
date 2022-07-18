51e850ca02f413f6f649a4eab0ac595cd503f936
Kshitiz Sharan <40540946+kshitizsharan@users.noreply.github.com>
2022-07-18 11:25:51 +0530

    do it

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from .routes import generator
-
-
-app = Flask(__name__,static_url_path="/tmp",static_folder="tmp")
-
-app.config.from_pyfile(config_file)
-
-app.register_blueprint(generator)
