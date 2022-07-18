609a3158f342234ad12fdee3759bcac2300ced7c
Kshitiz Sharan <40540946+kshitizsharan@users.noreply.github.com>
2022-07-18 11:26:31 +0530

    uploading

---
+++ routes.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template, request, redirect
+from generator import AI
+
+generator = Blueprint('generator',__name__)
+
+@generator.route('/')
+def index():
+    return render_template('index.html')
+
+@generator.route('/analyze',methods=['POST'])
+def analyze():
+    title = request.form["title"]
+    text = AI.generate_text(title)
+
+    return render_template('index.html', text=text)
